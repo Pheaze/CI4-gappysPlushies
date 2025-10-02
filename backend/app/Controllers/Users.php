f0ee29424a1093cb1a8a01493c7f4ff222412c89
Pheaze <202312154@fit.edu.ph>
2025-10-02 17:16:17 +0800

    feat added(moodboard and roadmap) -Moodboard and Roadmap as well as new routes

--- backend/app/Controllers/Users.php
+++ backend/app/Controllers/Users.php
@@ -10,4 +10,14 @@ class Users extends BaseController
     {
         return view('user/landingpage');
     }
+
+    public function moodBoard(): string
+    {
+        return view('user/mood');
+    }
+
+    public function roadMap(): string
+    {
+        return view('user/road');
+    }
 }
